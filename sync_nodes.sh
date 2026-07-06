a4315474c4aa4ae422b4ce80ed2c3e4c4ba7fe92
doug-barrett <doug.barrett@coalesce.io>
2026-07-06 12:04:38 -0600

    Sync nodes from /Users/dougy/work/POC/Omnia/migration_poc - 2026-07-06 12:04:38

---
+++ sync_nodes.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <source_folder>"
+    exit 1
+fi
+
+SOURCE_FOLDER="$1"
+
+if [ ! -d "$SOURCE_FOLDER/nodes" ]; then
+    echo "Error: $SOURCE_FOLDER/nodes does not exist"
+    exit 1
+fi
+
+rm -rf nodes
+cp -r "$SOURCE_FOLDER/nodes" nodes
+
+git add .
+git commit -m "Sync nodes from $SOURCE_FOLDER - $(date '+%Y-%m-%d %H:%M:%S')"
+git push origin
